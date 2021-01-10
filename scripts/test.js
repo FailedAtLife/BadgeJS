3498d438a4e249d0fddcd7c5cbbb2571a22b938e
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-10 14:00:18 +0100

    Create: test script

---
+++ scripts/test.js
@@ -0,0 +1,18 @@
+let start = new Date();
+const Badge = require('../lib/main');
+console.log('Imported BadgeJS module in', new Date() - start, 'ms');
+
+const fs = require('fs');
+
+start = new Date();
+let badge = Badge.from({
+    label: '<label_text>',
+    text: '<value_text>'
+});
+console.log('Created Badge object in', new Date() - start, 'ms');
+
+start = new Date();
+console.log(
+    // Do something...
+);
+console.log('Executed test script in', new Date() - start, 'ms');
