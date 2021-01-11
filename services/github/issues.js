f52f68b078709e662e5e8c72a3835a9acb2b3bce
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-11 13:11:11 +0100

    Update: issues service

--- services/github/issues.js
+++ services/github/issues.js
@@ -1,1 +1,30 @@
+const axios = require('axios');
 
+module.exports = {
+    args: 2,
+    main: async (args) => {
+        let user = args[0];
+        let repo = args[1];
+
+        console.log(`https://api.github.com/repos/${user}/${repo}/issues`);
+
+        let issues = await axios.get(`https://api.github.com/repos/${user}/${repo}/issues`, {
+            headers: {
+                Accept: 'application/vnd.github.v3+json'
+            }
+        });
+
+        let open = 0, closed = 0;
+
+        issues.data.forEach(issue => {
+            if (issue.open) open++;
+            else closed++;
+        });
+
+        return {
+            label: 'issues',
+            text: `${open} open, ${closed} closed`,
+            color: 'blue'
+        };
+    }
+}
