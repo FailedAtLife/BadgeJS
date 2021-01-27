53b237af6cfbe0124bf69f8f39eac668176b66ff
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-27 10:59:28 +0100

    Update: logger

--- core/logger.js
+++ core/logger.js
@@ -6,18 +6,57 @@
 
 'use strict';
 
+const chalk = require('chalk');
+
+/**
+ * Repeat a string x ammount of times
+ * 
+ * @method repeat
+ * @param {string} string Repeated string
+ * @param {number} x Times
+ * @returns {string} Repeated string
+ * @private
+ */
+function repeat(string, x) {
+    let res = '';
+    for (let i = 0; i < x; i++) {
+        res += string;
+    }
+
+    return res;
+}
+
 /**
  * Zero-pad a number in a string.
  * eg. 7 -> 07, but 17 -> 17
  *
  * @method pad
- * @param {string} string Input string
+ * @param {*} string Input
  * @returns {string} Padded string
  * @private
  */
-function pad(string) {
+function pad(string, len) {
     string = String(string);
-    return string.length < 2 ? '0' + string : string;
+    return string.length < len ?
+        repeat('0', len - string.length)
+        + string : string;
+}
+
+/**
+ * Get current time
+ * 
+ * @method date
+ * @returns {string} Time
+ * @private
+ */
+function date() {
+    const now = new Date();
+    let h = pad(now.getHours(), 2),
+        m = pad(now.getMinutes(), 2),
+        s = pad(now.getSeconds(), 2),
+        ms = pad(now.getMilliseconds(), 3);
+    
+    return `[ ${h}:${m}:${s}.${ms} ]`;
 }
 
 /**
@@ -31,6 +70,14 @@ function pad(string) {
  * @public
  */
 function logger(req, res, next) {
+    let addr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (addr.substring(0, 7) == '::ffff:')
+        addr = addr.substring(7);
+
+    console.log(`${addr} -- ${date()}`
+        + ' ' + chalk.keyword('orange').bold(req.method)
+        + ' ' + chalk.green(req.url));
+
     next();
     return true;
 }
