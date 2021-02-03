059b7c7e03ee011c7e38644541bfad8a10990fe4
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-02-03 19:40:05 +0100

    Refactor: server module

--- core/server.js
+++ core/server.js
@@ -7,83 +7,92 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const Badge = require('../lib/badge');
 
 let opt;
-module.exports = {
-    /**
-     * Sets multiple options for the server
-     *
-     * @method init
-     * @param {object} options Init options
-     * @returns {true}
-     * @public
-     */
-    init: function(options) {
-        if (options.production == undefined
-            || options.port == undefined)
-            throw new Error('Missing parameters');
 
-        opt = options;
+/**
+ * @constructor Server
+ * @public
+ */
+function Server() {
+    this.app = express();
+}
 
-        app.set('view engine', 'ejs');
-        app.set('views', 'frontend/pages');
-        app.use('/static', express.static('frontend/static'));
+/**
+ * Sets multiple options for the server
+ *
+ * @method Server.prototype.init
+ * @param {object} options Init options
+ * @returns {true}
+ * @public
+ */
+Server.prototype.init = function(options) {
+    if (options.production == undefined
+        || options.port == undefined)
+        throw new Error('Missing parameters');
 
-        if (opt.logger && !opt.production) app.use(opt.logger);
+    opt = options;
 
-        if (opt.production) {
-            app.use((req, res, next) => {
-                if (req.headers['x-forwarded-proto'] != 'https') {
-                    return res.redirect([
-                        'https://',
-                        req.get('Host'),
-                        req.url
-                    ].join(''));
-                } else next();
-            });
-        }
+    this.app.set('view engine', 'ejs');
+    this.app.set('views', 'frontend/pages');
+    this.app.use('/static/js', express.static('frontend/scripts'));
+    this.app.use('/static/css', express.static('frontend/styles'));
 
-        app.get('/', (req, res) => {
-            res.render('index');
+    if (opt.logger && !opt.production) this.app.use(opt.logger);
+
+    if (opt.production) {
+        this.app.use((req, res, next) => {
+            if (req.headers['x-forwarded-proto'] != 'https') {
+                return res.redirect([
+                    'https://',
+                    req.get('Host'),
+                    req.url
+                ].join(''));
+            } else next();
         });
-
-        app.get('/badge/*', (req, res) => {
-            let label = req.params[0].split('/')[0],
-                text = req.params[0].split('/')[1],
-                color = req.params[0].split('/')[2] || 'grey';
-            
-            try {
-                color = Badge.color(color);
-            } catch(e) {
-                color = Badge.color('grey');
-            }
-            
-            let badge = Badge.from({
-                label,
-                text,
-                color
-            });
-
-            res.setHeader('Content-Type', 'image/svg+xml');
-            res.send(badge.toString('svg'));
-        });
-
-        return true;
-    },
-
-    /**
-     * Starts the server
-     *
-     * @method start
-     * @returns {true}
-     * @public
-     */
-    start: function() {
-        if (opt.onstart) app.listen(opt.port, opt.onstart);
-        else app.listen(port);
-
-        return true;
     }
-};
+
+    this.app.get('/', (_, res) => {
+        res.render('index');
+    });
+
+    this.app.get('/badge/*', (req, res) => {
+        let label = req.params[0].split('/')[0],
+            text = req.params[0].split('/')[1],
+            color = req.params[0].split('/')[2] || 'grey';
+        
+        try {
+            color = Badge.color(color);
+        } catch(e) {
+            color = Badge.color('grey');
+        }
+        
+        let badge = Badge.from({
+            label,
+            text,
+            color
+        });
+
+        res.setHeader('Content-Type', 'image/svg+xml');
+        res.send(badge.toString('svg'));
+    });
+
+    return true;
+}
+
+/**
+ * Starts the server
+ *
+ * @method Server.prototype.start
+ * @returns {true}
+ * @public
+ */
+Server.prototype.start = function() {
+    if (opt.onstart) this.app.listen(opt.port, opt.onstart);
+    else this.app.listen(port);
+
+    return true;
+}
+
+module.exports = Server;
