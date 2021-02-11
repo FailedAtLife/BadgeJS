9e3a75aab403c59d068a4e62190502f46c870f28
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-02-11 10:44:39 +0100

    npm/files

---
+++ services/npm/files.js
@@ -0,0 +1,49 @@
+/**
+ * @module npm/files
+ * @author Robin Cunningham
+ * @version 1.0.0
+ */
+
+'use strict';
+
+const axios = require('axios');
+
+const mod = {};
+mod.async = true;
+mod.name = 'npm/files';
+mod.args = 1;
+
+/**
+ * Fetches file count from npm package
+ * 
+ * @method callback
+ * @param {array<string>} args URL arguments
+ * @param {BadgeJS} badge
+ * @public
+ */
+mod.callback = async function(args, badge) {
+    const pkg = args[0];
+
+    let files;
+    try {
+        let res = await axios.get(`https://registry.npmjs.org/${pkg}`, {
+            headers: {
+                Accept: 'application/vnd.npm.install-v1+json'
+            }
+        });
+
+        files = res.data.versions[res.data['dist-tags'].latest].dist.fileCount;
+    } catch (e) {
+        badge.label = 'files';
+        badge.text = 'not found';
+        badge.color = 'yellow';
+        return badge;
+    }
+
+    badge.label = 'files';
+    badge.text = files.toString();
+    badge.color = 'green';
+    return badge;
+}
+
+module.exports = mod;
