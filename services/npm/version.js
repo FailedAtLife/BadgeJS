a0b04cc29b3f717d841cbb6b515dbbb5f1bc7737
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-02-11 10:44:53 +0100

    npm/version

---
+++ services/npm/version.js
@@ -0,0 +1,49 @@
+/**
+ * @module npm/version
+ * @author Robin Cunningham
+ * @version 1.0.0
+ */
+
+'use strict';
+
+const axios = require('axios');
+
+const mod = {};
+mod.async = true;
+mod.name = 'npm/version';
+mod.args = 1;
+
+/**
+ * Fetches latest npm version from package
+ * 
+ * @method callback
+ * @param {array<string>} args URL arguments
+ * @param {BadgeJS} badge
+ * @public
+ */
+mod.callback = async function(args, badge) {
+    const pkg = args[0];
+
+    let version;
+    try {
+        let res = await axios.get(`https://registry.npmjs.org/${pkg}`, {
+            headers: {
+                Accept: 'application/vnd.npm.install-v1+json'
+            }
+        });
+
+        version = res.data['dist-tags'].latest;
+    } catch (e) {
+        badge.label = 'npm';
+        badge.text = 'not found';
+        badge.color = 'yellow';
+        return badge;
+    }
+
+    badge.label = 'npm';
+    badge.text = version;
+    badge.color = 'blue';
+    return badge;
+}
+
+module.exports = mod;
